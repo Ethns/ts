96b604a6fd910a5c61b797b74b6c7b793f6589c2
ethns <shenye601@hotmail.com>
2025-07-15 22:54:05 +1000

    format

--- server.js
+++ server.js
@@ -27,27 +27,27 @@ io.on('connection', (socket) => {
     console.log(`[Socket连接] Origin: ${origin}, ${socket.id}`);
 
     socket.on('joinRoom', ({ name, roomId }) => {
-    console.log(`[加入房间] 用户 ${name} 请求加入房间 ${roomId}`);
+        console.log(`[加入房间] 用户 ${name} 请求加入房间 ${roomId}`);
 
-    socket.join(roomId);
+        socket.join(roomId);
 
-    if (!roomUsers[roomId]) {
-        roomUsers[roomId] = [];
-    }
+        if (!roomUsers[roomId]) {
+            roomUsers[roomId] = [];
+        }
 
-    roomUsers[roomId].push({ socket, name });
+        roomUsers[roomId].push({ socket, name });
 
-    if (roomUsers[roomId].length === 2) {
-        const [player1, player2] = roomUsers[roomId];
+        if (roomUsers[roomId].length === 2) {
+            const [player1, player2] = roomUsers[roomId];
 
-        // 通知两人匹配成功
-        player1.socket.emit('matchSuccess', { opponent: player2.name });
-        player2.socket.emit('matchSuccess', { opponent: player1.name });
+            // 通知两人匹配成功
+            player1.socket.emit('matchSuccess', { opponent: player2.name });
+            player2.socket.emit('matchSuccess', { opponent: player1.name });
 
-        console.log(`[房间 ${roomId}] 对战开始：${player1.name} vs ${player2.name}`);
-    } else {
-        console.log(`[房间 ${roomId}] 等待第二位玩家...`);
-    }
+            console.log(`[房间 ${roomId}] 对战开始：${player1.name} vs ${player2.name}`);
+        } else {
+            console.log(`[房间 ${roomId}] 等待第二位玩家...`);
+        }
     });
 
     socket.on('gameUpdate', (data) => {
