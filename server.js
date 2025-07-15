4086b6eb1311f95d9c3796b4f1b6564dc05ce10f
ethns <shenye601@hotmail.com>
2025-07-15 21:26:58 +1000

    joinQueue

--- server.js
+++ server.js
@@ -21,16 +21,43 @@ const io = new Server(server, {
     },
 });
 
+const waitingPlayers = []; // 存储等待匹配的 { socket, name }
 // 房间机制
 io.on('connection', (socket) => {
     const origin = socket.handshake.headers.origin;
     console.log(`[Socket连接] Origin: ${origin}, ${socket.id}`);
 
-    socket.on('joinRoom', (roomId) => {
+    socket.on('joinQueue', ({ name }) => {
+    console.log(`[匹配] ${name} 请求加入队列`);
+
+    if (waitingPlayers.length > 0) {
+        const opponent = waitingPlayers.shift(); // 拿出排队最久的
+        const roomId = `room-${opponent.socket.id}-${socket.id}`;
+
+        // 让双方加入同一个房间
+        opponent.socket.join(roomId);
         socket.join(roomId);
-        console.log(`[房间] ${socket.id} 加入 ${roomId}`);
-        socket.to(roomId).emit('opponentJoined');
+
+        console.log(`[房间] 配对成功：${opponent.name} vs ${name} → ${roomId}`);
+
+        // 通知双方
+        opponent.socket.emit('matchFound', {
+        roomId,
+        opponent: name
+        });
+
+        socket.emit('matchFound', {
+        roomId,
+        opponent: opponent.name
+        });
+
+    } else {
+        // 没有其他玩家，当前玩家加入等待队列
+        waitingPlayers.push({ socket, name });
+        console.log(`[匹配] ${name} 正在等待配对...`);
+    }
     });
+
 
     socket.on('gameUpdate', (data) => {
         socket.to(data.roomId).emit('opponentUpdate', data);
